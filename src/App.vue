f6cc75d35788d6c12721b43474250d8088bd1098
RyanV-Souza <ryanvsouza99@gmail.com>
2022-03-08 10:48:59 -0300

    Add CSS and Studio Ghibli Logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <CardFilm
-      v-for="(film, index) in films"
-      :key="index"
-      :title="film.title"
-      :description="film.description"
-    > </CardFilm>
+    <img
+      src="./assets/logo.png"
+      alt=""
+    >
+    <div class="container">
+      <CardFilm
+        v-for="(film, index) in films"
+        :key="index"
+        :title="film.title"
+        :description="film.description"
+      > </CardFilm>
+    </div>
   </div>
 </template>
 
@@ -36,3 +42,25 @@ export default {
   },
 };
 </script>
+
+<style >
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+img {
+  display: block;
+  margin: 1rem auto;
+  max-width: 100%;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,12 +1,52 @@
 <template>
-	<div class="card">
-		<h1>{{title}}</h1>
-		<p>{{description}}</p>
-	</div>
+  <div class="card">
+    <h1>{{title}}</h1>
+    <p>{{description}}</p>
+  </div>
 </template>
 <script>
 export default {
-	name: 'CardFilm',
-	props: ['title', 'description']
-}
+  name: "CardFilm",
+  props: ["title", "description"],
+};
 </script>
+
+<style >
+.card {
+  margin: 1rem;
+  background: white;
+  border-radius: 10px;
+  overflow: hidden;
+  transition: all 0.2s linear;
+  box-shadow: 2px 4px 25px rgba(0, 0, 0, 0.1);
+}
+.card:hover {
+  box-shadow: 2px 8px 45px rgba(0, 0, 0, 0.15);
+  transform: translate3d(0, -2px, 0);
+}
+h1 {
+  text-align: center;
+  padding: 1.5rem 2.5rem;
+  background: black;
+  margin: 0 0 2rem 0;
+  font-size: 1.5rem;
+  color: white;
+}
+
+p {
+  padding: 0 2.5rem 2.5rem;
+  margin: 0;
+}
+
+@media screen and (min-width: 600px) {
+  .card {
+    flex: 1 1 calc(50% - 2rem);
+  }
+}
+
+@media screen and (min-width: 900px) {
+  .card {
+    flex: 1 1 calc(33% - 2rem);
+  }
+}
+</style>
