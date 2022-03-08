ff115432e67618b6fd18d4760f217333704cf452
RyanV-Souza <ryanvsouza99@gmail.com>
2022-03-08 10:29:45 -0300

    Creating CardFilm Component

---
+++ src/components/CardFilm.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="card">
+		<h1>{{title}}</h1>
+		<p>{{description}}</p>
+	</div>
+</template>
+<script>
+export default {
+	name: 'CardFilm',
+	props: ['title', 'description']
+}
+</script>
